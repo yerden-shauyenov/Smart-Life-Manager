55c986a2d9dce91c3975b2f786dadeae1c29dd39
Yerden <y_shauyenov@kbtu.kz>
2026-04-01 15:54:40 +0500

    New Structure

--- frontend/README.md
+++ frontend/README.md
@@ -1,4 +1,4 @@
-# SmartLifeManager
+# Frontend
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 21.1.3.
 
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('smart-life-manager');
+  protected readonly title = signal('frontend');
 }
