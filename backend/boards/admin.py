b4eae2385a7c5356ea9e8c34c3fe42a36bdd7cf4
Artyom <artemgorbachev046@gmail.com>
2026-04-14 17:40:41 +0500

    Change admins

--- backend/boards/admin.py
+++ backend/boards/admin.py
@@ -1,6 +1,61 @@
 from django.contrib import admin
-from .models import Board, BoardMembership, Task
+from .models import (
+    Board, BoardRole, BoardMembership,
+    TaskStatus, TaskPriority, TaskType,
+    Sprint, Task, Comment
+)
 
-admin.site.register(Board)
-admin.site.register(BoardMembership)
-admin.site.register(Task)
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'owner', 'is_public', 'created_at')
+    list_filter = ('is_public', 'created_at')
+    search_fields = ('title', 'owner__username')
+    ordering = ('-created_at',)
+
+@admin.register(Sprint)
+class SprintAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'board', 'is_active', 'start_date', 'end_date')
+    list_filter = ('is_active', 'board')
+    search_fields = ('name',)
+    ordering = ('-created_at',)
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'board', 'sprint', 'status', 'priority', 'author', 'assignee')
+    list_filter = ('board', 'status', 'priority', 'task_type')
+    search_fields = ('title', 'description')
+    ordering = ('-created_at',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'task', 'author', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('text',)
+    ordering = ('-created_at',)
+
+@admin.register(BoardRole)
+class BoardRoleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'board', 'can_manage_board')
+    list_filter = ('board', 'can_manage_board')
+
+@admin.register(BoardMembership)
+class BoardMembershipAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'board', 'role')
+    list_filter = ('board',)
+
+@admin.register(TaskStatus)
+class TaskStatusAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'board', 'order')
+    list_filter = ('board',)
+    ordering = ('board', 'order')
+
+@admin.register(TaskPriority)
+class TaskPriorityAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'board', 'level', 'color_hex')
+    list_filter = ('board',)
+    ordering = ('board', '-level')
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'board')
+    list_filter = ('board',)
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,52 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from django.contrib.auth.models import Group
+from rest_framework_simplejwt.token_blacklist.models import OutstandingToken, BlacklistedToken
 
-admin.site.register(User, UserAdmin)
+from .models import User
+from boards.models import Board, BoardMembership
+
+admin.site.unregister(Group)
+
+try:
+    admin.site.unregister(OutstandingToken)
+    admin.site.unregister(BlacklistedToken)
+except admin.sites.NotRegistered:
+    pass
+
+
+class OwnedBoardsInline(admin.TabularInline):
+    model = Board
+    extra = 0
+    fields = ('title', 'is_public', 'created_at')
+    readonly_fields = ('created_at',)
+    verbose_name = "Owned Board"
+    verbose_name_plural = "Owned Boards"
+
+
+class BoardMembershipsInline(admin.TabularInline):
+    model = BoardMembership
+    extra = 0
+    fields = ('board', 'role', 'joined_at')
+    readonly_fields = ('joined_at',)
+    verbose_name = "Board Membership"
+    verbose_name_plural = "Board Memberships"
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    inlines = [OwnedBoardsInline, BoardMembershipsInline]
+
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    readonly_fields = ('last_login', 'date_joined')
