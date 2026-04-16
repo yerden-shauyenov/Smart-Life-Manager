ab8f9bda440f94b7567aab6f63324b9e39c33d35
Artyom <artemgorbachev046@gmail.com>
2026-04-16 14:24:51 +0500

    Add error interceptor

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,12 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth.interceptor';
+import { errorInterceptor } from './interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(
+        withInterceptors([
+          authInterceptor,
+          errorInterceptor
+        ])
+    )
   ]
 };
---
+++ frontend/src/app/interceptors/error.interceptor.ts
@@ -0,0 +1,20 @@
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+    const router = inject(Router);
+    const authService = inject(AuthService);
+
+    return next(req).pipe(
+        catchError((error: HttpErrorResponse) => {
+            if (error.status === 401) {
+                authService.logout();
+                router.navigate(['/login']);
+            }
+            return throwError(() => error);
+        })
+    );
+};
