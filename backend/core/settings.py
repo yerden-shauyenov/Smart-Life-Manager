6184a269a85bb087538e9648addf973be6c0207c
Artyom <artemgorbachev046@gmail.com>
2026-04-20 21:39:12 +0500

    Add csrf trusted

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -86,6 +86,12 @@ CORS_ALLOWED_ORIGINS = [
     "https://www.podvallove.org"
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://api.podvallove.org",
+    "https://podvallove.org",
+    "https://www.podvallove.org"
+]
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'users.authentication.SessionTrackingJWTAuthentication',
