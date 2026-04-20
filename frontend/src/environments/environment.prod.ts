061c84a2b7aab49be0fe3b5024effff1509b9620
Artyom <artemgorbachev046@gmail.com>
2026-04-20 23:01:40 +0500

    Fix urls

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://api.podvallove.org/api',
+    production: true,
+    apiUrl: 'https://api.podvallove.org/api',
     imageBaseUrl: 'https://api.podvallove.org'
 };
